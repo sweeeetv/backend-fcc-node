06db487236f87b7dd5488410ade0a38cc5f5791a
sweeeetv <xgao566@aucklanduni.ac.nz>
2026-01-23 09:09:38 +0800

    node 4. serve static assets

--- myApp.js
+++ myApp.js
@@ -1,6 +1,7 @@
 let express = require("express");
 let app = express();
 
+app.use("/public", express.static(__dirname + "/public"));
 console.log("Hello World");
 app.get("/", (req, res) => {
   console.log("Someone just visited the home page!");
