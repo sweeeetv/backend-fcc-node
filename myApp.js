cff7a3bdf4ee80c0af35424027d077fa5795ba3b
sweeeetv <xgao566@aucklanduni.ac.nz>
2026-01-23 08:33:33 +0800

    node 2. use node --watch server.js to track the changes

--- myApp.js
+++ myApp.js
@@ -3,6 +3,7 @@ let app = express();
 
 console.log("Hello World");
 app.get("/", (req, res) => {
-  res.send("Hello from Azure! The wiring is fixed.");
+  console.log("Someone just visited the home page!");
+  res.send("Hello Express");
 });
 module.exports = app;
