eec515ac98cc7ab99951fd74da677e1fd4dea5db
sweeeetv <xgao566@aucklanduni.ac.nz>
2026-01-24 12:19:05 +0800

    node 5. rest api, serving json

--- myApp.js
+++ myApp.js
@@ -7,4 +7,7 @@ app.get("/", (req, res) => {
   console.log("Someone just visited the home page!");
   res.sendFile(__dirname + "/views/index.html");
 });
+app.get("/json", (req, res) => {
+  res.json({ message: "Hello json" });
+});
 module.exports = app;
