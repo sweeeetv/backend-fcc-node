ee5da0d143ec3bf32e699f2b54edbc5b402b4365
sweeeetv <xgao566@aucklanduni.ac.nz>
2026-01-24 14:01:29 +0800

    node 7. middle ware

--- myApp.js
+++ myApp.js
@@ -1,13 +1,21 @@
 require("dotenv").config();
 let express = require("express");
 let app = express();
+console.log("Hello World");
+
+//this line if not for human, but for HTML to find the css file
+app.use("/", (req, res, next) => {
+  console.log(req.method + " " + req.path + " - " + req.ip);
+  next();
+});
 
 app.use("/public", express.static(__dirname + "/public"));
-console.log("Hello World");
+
 app.get("/", (req, res) => {
   console.log("Someone just visited the home page!");
   res.sendFile(__dirname + "/views/index.html");
 });
+
 app.get("/json", (req, res) => {
   let message = "Hello json";
   if (process.env.MESSAGE_STYLE === "uppercase") {
@@ -15,4 +23,5 @@ app.get("/json", (req, res) => {
   }
   res.json({ message: message });
 });
+
 module.exports = app;
